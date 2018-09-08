51b82875cb326bd4c2d9997a1945916ac2fba6fd
Petri Autio <petri.mikael.autio@gmail.com>
2018-09-08 22:58:29 +0100

    UI tweaks

--- Assets/Scripts/AppController.cs
+++ Assets/Scripts/AppController.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppController : MonoBehaviour {
-
+    // Icon made by Freepik from www.flaticon.com 
 
     // Need a data structure for this to parse and fill out texts accordingly. 
     // This needs to be thought through.
     // Main level, sublevel1, sublevel2
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
